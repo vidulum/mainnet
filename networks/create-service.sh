8d7d5f8453c1c99965a1dde337132be7ff27c943
Corey-Code <37006206+Corey-Code@users.noreply.github.com>
2021-10-26 22:12:09 -0500

    fix: build service

--- networks/create-service.sh
+++ networks/create-service.sh
@@ -6,7 +6,7 @@ BASEDIR=$(dirname "$0")
 
 VIDULUMD_BINARY=$(which vidulumd || (echo -e "\033[31mPlease add vidulumd to PATH\033[0m" 1>&2 && exit 1))
 VIDULUMD_USER=$USER
-VIDULUMD_BINARY_DIR=$(dirname $(which vidulumd))
+VIDULUMD_BINARY_DIR=$(dirname $(which vidulum))
 VIDULUMD_USER_HOME=$(eval echo "~$USER")
 
 sed "s#<VIDULUMD_BINARY>#$VIDULUMD_BINARY#g; s#<VIDULUMD_USER>#$VIDULUMD_USER#g; s#<VIDULUMD_BINARY_DIR>#$VIDULUMD_BINARY_DIR#g; s#<VIDULUMD_USER_HOME>#$VIDULUMD_USER_HOME#g" $BASEDIR/vidulumd.service.template > $BASEDIR/vidulumd.service
